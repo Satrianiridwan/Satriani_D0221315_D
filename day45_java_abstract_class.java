7b272fd2f96e43fa77a0083a5f83cf421b4d38f0
Satrianiridwan <99650721+Satrianiridwan@users.noreply.github.com>
2022-04-06 10:28:55 +0700

    day45_java_abstract_class.java

---
+++ day45_java_abstract_class.java
@@ -0,0 +1,19 @@
+import java.util.*;
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
+	
+}
+class MyBook extends Book{
+    @Override
+    void setTitle(String s){
+        title = s;
+    }
+}
+
+
+
+public 
